f88f9b2420c259b6f194fbbc2827036d8a51b428
skarolk <skarolkiewicz@gmail.com>
2021-01-14 19:43:16 -0600

    updated readme

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,20 @@ var path = require('path');
 module.exports = {
     mode: 'production',
     entry: './src/GoogleOAuth.js',
+    externals: {
+        react: {
+            root: 'React',
+            commonjs2: 'react',
+            commonjs: 'react',
+            amd: 'react'
+        },
+        'react-dom': {
+            root: 'ReactDOM',
+            commonjs2: 'react-dom',
+            commonjs: 'react-dom',
+            amd: 'react-dom'
+        }
+    },
     output: {
         path: path.resolve('lib'),
         filename: 'GoogleOAuth.js',
